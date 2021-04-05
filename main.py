e3afcfd828c6b38e4c53ff476a12b850dc25244b
huantianad <davidtianli@gmail.com>
2021-04-04 20:31:06 -0700

    Switch to alive-progress instead of clint, clean up code

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import os
 import re
 from multiprocessing.pool import ThreadPool
 
+from alive_progress import alive_bar
 import requests
-from clint.textui import progress
+
 
 level_path = "downloader"
 
@@ -15,52 +16,69 @@ def pr_orange(skk): print(f"\033[33m{skk}\033[00m")
 def pr_red(skk): print(f"\033[31m{skk}\033[00m")
 
 
-# Iterate through possible renames until valid name
-def rename(name, index):
-    name = name.split('.rdzip')[0]
-    if os.path.exists(f'{level_path}/{name} ({index}).rdzip'):
-        return rename(name, index + 1)
+def rename(path: str):
+    if os.path.exists(path):
+        index = 1
+        path = path.replace(".rdzip", "")  # Gets rid of the .rdzip extension, we add it back later on.
+        while os.path.exists(f"{path} ({index}).rdzip"):
+            index += 1
+        return f"{path} ({index}).rdzip"
     else:
-        return f"{name} ({index})" + ".rdzip"
+        # When the file doesn't exist, we don't need to do anything, so we can just directly return the filename
+        return path
 
 
-def download(url):
-    if url.startswith('https://drive.google.com/') or url.startswith("https://www.dropbox.com/s/"):
-        r = requests.get(url)
-        name = re.findall('filename=(.+)', r.headers.get('Content-Disposition'))[0].split(";")[0].replace('"', "")
-    else:
+def get_url_filename(url: str) -> str:
+    if url.endswith('.rdzip'):
+        # When the filename already ends with the file extension, we can just snatch it from the url
         name = url.split('/')[-1]
+    else:
+        # Otherwise, we need to use some weird stuff to get it from the Content-Disposition header
+        r = requests.get(url).headers.get('Content-Disposition')
+        name = re.findall('filename=(.+)', r)[0].split(";")[0].replace('"', "")
 
-    if os.path.exists(f'{level_path}/{name}'):
-        name = rename(name, 1)
-
-    r = requests.get(url, stream=True)
-    with open(f'{level_path}/{name}', 'wb') as f:
-        for ch in r:
-            f.write(ch)
+    # Remove the characters that windows doesn't like in filenames
+    for char in r'<>:"/\|?* ':
+        name = name.replace(char, '')
 
     return name
 
 
-# Checks if the level given is verified
-def check_verified(level):
-    try:
-        return level['verified']
-    except KeyError:
-        return False
+def download_level(url: str):
+    # Get the proper filename of the level, append it to the path to get the full path to the downloaded level.
+    filename = get_url_filename(url)
+    full_path = os.path.join(level_path, filename)
+
+    # Get a unique file name
+    full_path = rename(full_path)
+
+    # Download the level
+    r = requests.get(url, stream=True)
+    if r.status_code == 200:
+        with open(full_path, 'wb') as file:
+            for chunk in r:
+                file.write(chunk)
+    else:
+        pr_red(f'''ERROR: Status code not 200 when downloading {full_path}, maybe the level was deleted on discord?
+               Please tell a mod about this. level url: {url}''')
+
+    return full_path  # Returns the final path to the downloaded level
 
 
 def main():
     if not os.path.exists(f'{level_path}/'):
         os.mkdir(f'{level_path}/')
-        pr_orange("Created download folder.")
+        pr_green(f'Created folder {level_path}, saving levels there.')
+    else:
+        pr_red(f'ERROR: folder "{level_path}" already exists! Please remove and try again.')
+        return
 
-    pr_cyan("Accessing website.")
+    pr_cyan("Accessing website...")
     url = 'https://script.google.com/macros/s/AKfycbzm3I9ENulE7uOmze53cyDuj7Igi7fmGiQ6w045fCRxs_sK3D4/exec'
     site_data = requests.get(url).json()
     pr_green("Success!")
 
-    # Allow user to decide whether to download checked or all levels
+    # Prompt user for what levels to download
     while True:
         ask_checked = input("Would you like to download (a)ll or (c)hecked levels? ")
         if ask_checked.lower() in ['a', 'all']:
@@ -68,17 +86,19 @@ def main():
             pr_cyan("Downloading all levels...")
             break
         elif ask_checked.lower() in ['c', 'checked']:
-            site_urls = [x['download_url'] for x in site_data if check_verified(x)]
+            site_urls = [x['download_url'] for x in site_data if x.get('verified')]
             pr_cyan("Downloading checked levels...")
             break
         else:
-            pr_red(f"ERROR: \"{ask_checked}\" is not a valid option.")
+            pr_red(f'ERROR: "{ask_checked}" is not a valid option.')
 
     pr_cyan(f"Total levels found: {len(site_urls)}\n")
 
-    results = ThreadPool(8).imap_unordered(download, site_urls)
-    for chunk in progress.bar(results, expected_size=len(site_urls)):
-        pr_green(f"Downloaded {chunk}".ljust(55))
+    results = ThreadPool(8).imap_unordered(download_level, site_urls)
+    with alive_bar(len(site_urls), spinner='notes_scrolling') as bar:
+        for result in results:
+            pr_green(f"Downloaded {result}")
+            bar()
 
     input("Done downloading! Press Enter to continue...")
 
