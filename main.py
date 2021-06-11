2c4e6d5047a13f10968cacbca5efd0a788169734
huantianad <davidtianli@gmail.com>
2021-06-10 20:34:58 -0700

    Add thread user input

--- main.py
+++ main.py
@@ -2,64 +2,58 @@ import os
 import re
 from multiprocessing.pool import ThreadPool
 
-from alive_progress import alive_bar
+import colorama
 import requests
-from colorama import init
+from alive_progress import alive_bar
 from termcolor import cprint
 
 
-level_path = "downloader"
-
-
-# windows is dumb, color fixes
-init()
-is_windows = os.name == 'nt'
-
-
-def rename(path: str):
+def rename(path: str) -> str:
     if os.path.exists(path):
-        index = 1
-        path = path.replace(".rdzip", "")  # Gets rid of the .rdzip extension, we add it back later on.
-        while os.path.exists(f"{path} ({index}).rdzip"):
+        index = 2
+        extension = '.' + path.rsplit('.', 1)[-1]
+        path = path.replace(extension, '')  # Gets rid of the .rdzip extension, we add it back later on.
+
+        while os.path.exists(f"{path} ({index}){extension}"):
             index += 1
-        return f"{path} ({index}).rdzip"
+
+        return f"{path} ({index}){extension}"
     else:
-        # When the file doesn't exist, we don't need to do anything, so we can just directly return the filename
         return path
 
 
-def get_url_filename(url: str) -> str:
+def get_url_filename(url: str) -> tuple[str, requests.Response]:
     if url.endswith('.rdzip'):
         # When the filename already ends with the file extension, we can just snatch it from the url
         name = url.split('/')[-1]
     else:
         # Otherwise, we need to use some weird stuff to get it from the Content-Disposition header
-        r = requests.get(url).headers.get('Content-Disposition')
-        name = re.findall('filename=(.+)', r)[0].split(";")[0].replace('"', "")
+        r = requests.get(url, allow_redirects=True, stream=True)
+        h = r.headers.get('Content-Disposition')
+        name = re.findall('filename="(.+)"', h)[0]
 
     # Remove the characters that windows doesn't like in filenames
-    for char in r'<>:"/\|?* ':
+    for char in r'<>:"/\|?*':
         name = name.replace(char, '')
 
-    return name
+    return name, r
 
 
 def download_level(url: str):
     # Get the proper filename of the level, append it to the path to get the full path to the downloaded level.
-    filename = get_url_filename(url)
-    full_path = os.path.join(level_path, filename)
+    filename, r = get_url_filename(url)
+    full_path = os.path.join(DOWNLOAD_PATH, filename)
 
     # Get a unique file name
     full_path = rename(full_path)
 
     # Download the level
-    r = requests.get(url, stream=True)
     if r.status_code == 200:
         with open(full_path, 'wb') as file:
             for chunk in r:
                 file.write(chunk)
     else:
-        if is_windows:
+        if IS_WINDOWS:
             print(f'''ERROR: Status code not 200 when downloading {full_path}, maybe the level was deleted on discord?
                    Please tell a mod about this. level url: {url}''')
         else:
@@ -70,26 +64,48 @@ def download_level(url: str):
 
 
 def main():
-    if not os.path.exists(f'{level_path}/'):
-        os.mkdir(f'{level_path}/')
-        cprint(f'Created folder {level_path}, saving levels there.', 'green')
+    global DOWNLOAD_PATH
+    global IS_WINDOWS
+    DOWNLOAD_PATH = "downloader/"
+    IS_WINDOWS = os.name == 'nt'
+
+    # windows is dumb, color fixes
+    colorama.init()
+
+    if not os.path.exists(DOWNLOAD_PATH):
+        os.mkdir(DOWNLOAD_PATH)
+        cprint(f'Created folder {os.path.abspath(DOWNLOAD_PATH)}, saving levels there.', 'green')
     else:
-        cprint(f'ERROR: folder "{level_path}" already exists! Please remove and try again.', 'red')
-        return
+        cprint(f'Warning: folder "{os.path.abspath(DOWNLOAD_PATH)}" already exists!', 'yellow')
 
     cprint("Accessing website...", 'cyan')
     url = 'https://script.google.com/macros/s/AKfycbzm3I9ENulE7uOmze53cyDuj7Igi7fmGiQ6w045fCRxs_sK3D4/exec'
     site_data = requests.get(url).json()
     cprint("Success!", 'green')
 
+    # Prompt user for number of download threads
+    while True:
+        threads = input("How many download threads would you like to use? (Default: 8) ")
+
+        if not threads:
+            threads = 8
+            break
+
+        try:
+            threads = int(threads)
+        except ValueError:
+            cprint(f'ERROR: "{threads}" is not a valid integer!')
+        else:
+            break
+
     # Prompt user for what levels to download
     while True:
-        ask_checked = input("Would you like to download (a)ll or (c)hecked levels? ")
-        if ask_checked.lower() in ['a', 'all']:
+        ask_checked = input("Would you like to download checked levels only? (Y/n) ")
+        if ask_checked.lower() in ['n', 'no']:
             site_urls = [x['download_url'] for x in site_data]
             cprint("Downloading all levels...", 'cyan')
             break
-        elif ask_checked.lower() in ['c', 'checked']:
+        elif ask_checked.lower() in ['y', 'yes', '']:
             site_urls = [x['download_url'] for x in site_data if x.get('verified')]
             cprint("Downloading checked levels...", 'cyan')
             break
@@ -98,10 +114,10 @@ def main():
 
     cprint(f"Total levels found: {len(site_urls)}\n", 'cyan')
 
-    results = ThreadPool(8).imap_unordered(download_level, site_urls)
+    results = ThreadPool(threads).imap_unordered(download_level, site_urls)
     with alive_bar(len(site_urls), spinner='notes_scrolling') as bar:
         for result in results:
-            if is_windows:
+            if IS_WINDOWS:
                 print(f"Downloaded {result.ljust(76)}")
             else:
                 cprint(f"Downloaded {result}", 'green')
