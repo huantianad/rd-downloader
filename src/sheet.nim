293314a66623fc7c891730d71711c9ab56963fd3
huantian <davidtianli@gmail.com>
2022-05-06 16:29:06 -0700

    Move fully to orchard!

--- src/sheet.nim
+++ src/sheet.nim
@@ -1,34 +1,18 @@
 import std/[
-  asyncdispatch,
   db_sqlite,
   httpclient,
   net,
-  os,
   options,
+  os,
   sequtils,
   tempfiles,
   uri,
-  strutils,
 ]
-
-type
-  LevelData = object
-    downloadUrl: Uri
-    verified: Option[bool]
 
 proc open(connection: string): DbConn =
   open(connection, "", "", "")
 
-proc sqlRowToLevelData(row: Row): LevelData =
-  result.downloadUrl = row[0].parseUri()
-
-  let verifiedInt = row[1].parseInt()
-  result.verified =
-    if verifiedInt > 0: some(true)
-    elif verifiedInt < 0: some(false)
-    else: none[bool]()
-
-proc getSiteData(client: HttpClient): seq[LevelData] {.gcsafe.} =
+proc getSiteUrls(client: HttpClient, verifiedOnly: bool): seq[Uri] {.gcsafe.} =
   let tempDir = createTempDir("rddownloader_", "")
   defer: removeDir(tempDir)
 
@@ -43,25 +27,24 @@ proc getSiteData(client: HttpClient): seq[LevelData] {.gcsafe.} =
   db.exec(sql"ATTACH DATABASE ? as status", tempDir / "status.db")
   db.exec(sql"""
     CREATE TEMP VIEW levels AS
-    SELECT level.url, status.approval FROM level
-    INNER JOIN status ON status.id = level.id
+      SELECT
+        level.url,
+        COALESCE(status.approval, 0) AS approval
+      FROM
+        level
+      LEFT JOIN status ON status.id = level.id
   """)
 
-  let raw = db.getAllRows(sql"SELECT * from levels")
-  result = raw.map(sqlRowToLevelData)
+  let statement =
+    if verifiedOnly:
+      sql"SELECT url from levels WHERE approval > 0"
+    else:
+      sql"SELECT url from levels"
+
+  db.getAllRows(statement).mapIt(it[0].parseUri)
 
 proc getSiteUrls*(verifiedOnly: bool): seq[Uri] {.gcsafe.} =
   var client = newHttpClient()
-  var data = client.getSiteData()
+  defer: client.close()
 
-  if verifiedOnly:
-    data = data.filterIt(it.verified.get(false))
-
-  return data.mapIt(it.downloadUrl)
-
-when isMainModule:
-  # let client = newHttpClient()
-  # discard client.getSiteData()
-
-  echo getSiteUrls(true).len
-  echo getSiteUrls(false).len
+  client.getSiteUrls(verifiedOnly)
